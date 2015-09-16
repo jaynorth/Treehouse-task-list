27a304c981faa630f0cb747c23e311b77ce7ff6d
jayweb <jayrdee@gmail.com>
2015-09-16 11:41:39 +0200

    update

---
+++ js/app.js
@@ -0,0 +1,29 @@
+
+
+
+
+var taskInput = document.getElementById('new-task');
+console.log(taskInput);
+
+var addTask = function (){
+
+
+}
+var deleteTask = function(){
+
+
+}
+var editTask = function(){
+
+}
+
+var completedTask = function(){
+
+
+}
+
+var incompleteTask = function(){
+
+}
+
+
