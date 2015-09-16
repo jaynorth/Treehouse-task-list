306c3f963b1cd2da7617f52e7ef47a23e50be644
jayweb <jayrdee@gmail.com>
2015-09-16 12:00:58 +0200

    updated JS

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
 
 
-
+//Variables targeting the elements
 
 var taskInput = document.getElementById('new-task');
 console.log(taskInput);
 
+var addButton = document.getElementsByTagName("button")[0]; //in an array for buttons
+
+var incompleteTaskHolder = document.getElementById('incomplete-tasks');
+
+var completedTaskHolder = document.getElementById('completed-tasks');
+
+
+// Functions required
 var addTask = function (){
 
 
