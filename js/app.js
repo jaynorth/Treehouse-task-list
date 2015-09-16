6ca4cdc17b0ca3c059da0ccc14fc8eb08d962ecf
jayweb <jayrdee@gmail.com>
2015-09-16 14:26:11 +0200

    again

--- js/app.js
+++ js/app.js
@@ -14,24 +14,35 @@ var completedTaskHolder = document.getElementById('completed-tasks');
 
 // Functions required
 var addTask = function (){
-
+console.log("Add Task ..");
 
 }
 var deleteTask = function(){
-
+console.log("deleting Task ..");
 
 }
 var editTask = function(){
-
+console.log("Editing Task ..");
 }
 
 var completedTask = function(){
-
+console.log(" Task complete ..");
 
 }
 
 var incompleteTask = function(){
-
+console.log("Task incomplete..");
 }
+
+
+//
+var bindTaskEvents = function (taskListItem, checkBoxEventHandler){
+
+};
+
+// Events
+
+addButton.onclick = addTask ;// We don't use addTask() because it's set as a variable, 
+//if we did then the function would be triggered the first time without needing to click 
 
 
